764b9284068b42c701d1af2ca182a7025ea94acf
Sinthia03 <siddikasinthia@gmail.com>
2025-11-17 10:53:28 +0600

    labtask02

--- MID/labtask02.php
+++ MID/labtask02.php
@@ -4,7 +4,29 @@
    
     <title>Management Company</title>
 </head>
+<style>
+    body {
+        background-color:RosyBrown;
+
+    }
+    
+    div{
+  width:600px;
+  height: 950px;
+  padding:10px;
+  border:3px solid black ;
+  margin:0;
+ background-color:white;
+    }
+</style>
+
 <body>
     
+
+
+
+
+
+
 </body>
 </html>
