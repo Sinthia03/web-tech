8a8ba6d38ae0cc9b91d3974028ce452779515588
Sinthia03 <siddikasinthia@gmail.com>
2025-12-13 21:21:06 +0600

    Add new files

---
+++ Project/Login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+</head>
+<body>
+<div>
+  
+    <center>
+         <h1>Login Now</h1>
+        <form method = "post" action ="">
+            <label>Enter your name:</label>
+   <input type ="text" id ="name "  name ="name">
+   <br><br>
+   <label>Enter your password:</label>
+    <input type ="text" id ="password"  name ="password">
+    <br><br>
+ <input type = "submit" name ="submit" value ="Login">
+<br><br>
+<a href ="#">Forgot Password ?</a>
+</form>
+
+</center>
+
+</div>
+</body>
+</html>
