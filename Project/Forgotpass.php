33aecaca8adf4a6e9c641e6bd02be39e1f824af8
Sinthia03 <siddikasinthia@gmail.com>
2025-12-20 20:33:26 +0600

    commit

--- Project/Forgotpass.php
+++ Project/Forgotpass.php
@@ -9,22 +9,22 @@
 
 <body>
  <center>
-     <form>
+     <form method ="post" action ="Forgetpassvalidate.php">
     <h1>Forgot Pass ?</h1>
    <p> Enter a new password to change your password  </p>
    
  <label>New Password:</label>
-<input type ="text" id="newpassword" placeholder="Must be 8 character long"/>
+<input type ="text"  name="newpassword" id="newpassword" placeholder="Must be 8 character long"/>
 <br><br>
 <br><br>
 
 
 <label>Confirm New Password:</label>
-<input type ="text" id="confirmpassword" />
+<input type ="text" name="confirmpassword" id="confirmpassword" />
 <br><br>
 <br><br>
 
-<button type ="button">Back </button>
+<button type ="button"  onclick="window.location.href='Login.php';">Back </button>
 <button type ="submit">Submit </button>
 
 
