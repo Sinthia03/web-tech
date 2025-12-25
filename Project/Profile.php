251bdf77733cb463863fa96332343ba8fd756216
Sinthia03 <siddikasinthia@gmail.com>
2025-12-25 23:05:32 +0600

    commit

--- Project/Profile.php
+++ Project/Profile.php
@@ -3,9 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Customer Profile</title>
+    <link rel= "stylesheet" href="Profile.css">
 </head>
 <body>
+<dciv class =" profile-card">
+<h1>Welcome To Profile</h1>
+<div class ="profile-actions">
+    <button>Edit Profile</button>
+    <button>Update Password</button><br><br>
+    <button class ="secondary-btn" onclick ="window.location.href='userdashboard.php'"> Back to Dashboard</button>
+</div>
+
+
+
+</div>
     
 </body>
 </html>
