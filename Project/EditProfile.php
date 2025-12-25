e194d2e574a12ab68d056af820725e8472a9727c
Sinthia03 <siddikasinthia@gmail.com>
2025-12-26 00:16:29 +0600

    commit

--- Project/EditProfile.php
+++ Project/EditProfile.php
@@ -3,9 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Edit Profile</title>
+    <link rel="stylesheet" href="EditProfile.css">
+
 </head>
 <body>
+<form id ="editprofile" class ="edit-card" method ="post">
+    <h1>Edit Profile!</h1>
+    
+
+
+
+
     
 </body>
 </html>
