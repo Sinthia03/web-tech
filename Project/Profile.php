96f6aff7ed5b995bad2561122dcd2935cf4d7171
Sinthia03 <siddikasinthia@gmail.com>
2025-12-25 23:15:12 +0600

    commit

--- Project/Profile.php
+++ Project/Profile.php
@@ -9,6 +9,8 @@
 <body>
 <dciv class =" profile-card">
 <h1>Welcome To Profile</h1>
+<img src ="https://plus.unsplash.com/premium_photo-1677252438411-9a930d7a5168?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dXNlciUyMHByb2ZpbGUlMjBpY29ufGVufDB8fDB8fHww" alt ="Profile Picture" class ="avatar">
+
 <div class ="profile-actions">
     <button>Edit Profile</button>
     <button>Update Password</button><br><br>
