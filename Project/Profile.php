6e89893cc7c351b690fd368700930350be1a2acc
Sinthia03 <siddikasinthia@gmail.com>
2025-12-26 00:53:09 +0600

    commit

--- Project/Profile.php
+++ Project/Profile.php
@@ -12,7 +12,7 @@
 <img src ="https://plus.unsplash.com/premium_photo-1677252438411-9a930d7a5168?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dXNlciUyMHByb2ZpbGUlMjBpY29ufGVufDB8fDB8fHww" alt ="Profile Picture" class ="avatar">
 
 <div class ="profile-actions">
-    <button>Edit Profile</button>
+    <button  onclick="window.location.href='EditProfile.php'">Edit Profile</button>
     <button>Update Password</button><br><br>
     
 </div>
