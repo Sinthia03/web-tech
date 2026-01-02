a04d580f133b7a987f8ce8fa3b6a554d959ba52a
Sinthia03 <siddikasinthia@gmail.com>
2026-01-03 00:14:08 +0600

    commit

--- Project/BookRoom.php
+++ Project/BookRoom.php
@@ -3,9 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>View Rooms</title>
+    <script>
+        function submitFilters() {
+            document.getElementById('filterForm').submit();
+        }
+
+        function resetFilters() {
+            document.getElementById('filterForm').reset();submitFilters();
+        }
+        </script>
 </head>
 <body>
+    <>
     
 </body>
 </html>
