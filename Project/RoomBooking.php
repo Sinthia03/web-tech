9504174de9d850362c3234513d195aa7eab66f81
Sinthia03 <siddikasinthia@gmail.com>
2026-01-03 21:56:43 +0600

    com

--- Project/RoomBooking.php
+++ Project/RoomBooking.php
@@ -4,8 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="RoomBooking.css">
 </head>
 <body>
+
+<div class ="container">
+    <h1> Room 101_Delux </h1>
+    <img src = "" alt ="Room image">
+<p><strong>Price Per Night : </strong> TK 3500.00</p>
+<p>Status:</strong>
+<span class ="badge available" >Available</span>
+</p>
+
+<p><strong>Features:</strong>
+
+
     
 </body>
 </html>
