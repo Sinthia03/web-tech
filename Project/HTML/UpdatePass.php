af346a127a4cf8658d6ade35d947c583b96a33c2
Sinthia03 <siddikasinthia@gmail.com>
2026-01-03 23:33:04 +0600

    com

--- Project/HTML/UpdatePass.php
+++ Project/HTML/UpdatePass.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,6 +7,22 @@
      <link rel="stylesheet" href="../CSS/UpdatePass.css">
 </head>
 <body>
+    <div class="password-card">
+        <h1>Update Your Password Here! </h1>
+        <form>
+
+<label >Old Password:</label>
+<input type="password" id="oldPass" name="old_password">
+<p class="error-msg"></p>
+
+
+
+
+
+
+
+
+</form>    
     
 </body>
 </html>
