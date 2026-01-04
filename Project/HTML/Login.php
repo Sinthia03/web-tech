b214cfafbce112f06d2ab95cad1e269cdcf97bd6
Sinthia03 <siddikasinthia@gmail.com>
2026-01-04 14:50:51 +0600

    com

--- Project/HTML/Login.php
+++ Project/HTML/Login.php
@@ -7,39 +7,12 @@
      <link rel="stylesheet" href="../CSS/login.css">
 </head>
 <body>
-<div>
-  
-    <center>
-         <h1>Login Now</h1>
-         </center>
-        <form method = "post" action ="">
-            <div>
-            <label>Enter your name:</label>
-   <input type ="text"  name="name" value="<?php echo $name; ?>">
-   <?php echo $nameError; ?>
-    <br><br>
-</div>
-
- <div>
-   <label>Enter your password:</label>
-    <input type ="text" name="password"  value= "">
-   
-    <br><br>
-</div>
-    <center>
- <div>
- <input type ="submit" name="submit" value ="Login">
-<br><br>
-</div>
-
- <div>
-<a href ="#">Forgot Password ?</a>
-</div>
-</center>
-</form>
+<div class ="login-box">
+    
 
 
 
-</div>
+
+
 </body>
 </html>
